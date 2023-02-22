ca921414af42e39e20c3d01e0489273c55bbb6d2
Kydriaezh <bazanova.nata@gmail.com>
2023-02-22 11:09:11 +0300

    homework3

---
+++ homework3/task3.py
@@ -0,0 +1,8 @@
+
+dict = {"AEIOULNSTRАВЕИНОРСТ": 1, "DGДКЛМПУ": 2, "BCMPБГЁЬЯ": 3, "FHVWYЙЫ": 4, "KЖЗХЦЧ": 5,
+            "JXШЭЮ": 8, "QZФЩЪ": 10}
+
+word = input()
+print(sum([i[1] for i in dict.items() for j in word if j.upper() in i[0]]))
+
+
